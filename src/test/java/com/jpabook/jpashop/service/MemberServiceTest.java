87f865b651e30051f70de20d0ce44dbdf9b563eb
dayday <digetlyn@naver.com>
2024-10-05 11:21:26 +0900

    241005

--- src/test/java/com/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/com/jpabook/jpashop/service/MemberServiceTest.java
@@ -3,6 +3,7 @@ package com.jpabook.jpashop.service;
 import com.jpabook.jpashop.domain.Member;
 import com.jpabook.jpashop.repository.MemberRepository;
 import jakarta.persistence.EntityManager;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,15 +41,23 @@ public class MemberServiceTest {
     }
 
 
-    @Test
+    @Test(expected = IllegalStateException.class)
     public void 중복_회원_예외() throws Exception{
         //given
+        Member member1 = new Member();
+        member1.setName("SONG2");
+
+        Member member2 = new Member();
+        member2.setName("SONG3");
 
 
         //when
+        memberService.join(member1);
+        memberService.join(member2); //예외가 발생해야 한다.!!!
 
 
         //then
+        Assert.fail("예외가 발생해야 한다.");
     }
 
 }
