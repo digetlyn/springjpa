b28adfbfc8e90512a63b8467f3fef77aae636043
dayday <digetlyn@naver.com>
2024-10-05 13:12:21 +0900

    Member add test

--- src/test/java/com/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/com/jpabook/jpashop/service/MemberServiceTest.java
@@ -14,9 +14,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@Transactional
+@RunWith(SpringRunner.class)//스프링과 테스트 통합
+@SpringBootTest //스프링 부트 띄우고 테스트(이게 없으면 @Autowired 다실패)
+@Transactional //반복 가능한 테스트 지원, 가각의 테스트를 실행할 때마다 트랜잭션을 시작하고 테스트가 끝나면 트랜잭션을 강제로 롤백( 이 어노테이션이 테스트 케이스에서 사용될 때만 롤백)
 public class MemberServiceTest {
 
     @Autowired MemberService memberService;
@@ -24,6 +24,7 @@ public class MemberServiceTest {
     @Autowired EntityManager em;
 
     @Test
+    @Rollback(value = false)
     public  void  회원가입() throws Exception{
         //given
         Member member = new Member();
@@ -48,7 +49,7 @@ public class MemberServiceTest {
         member1.setName("SONG2");
 
         Member member2 = new Member();
-        member2.setName("SONG3");
+        member2.setName("SONG2");
 
 
         //when
@@ -57,7 +58,7 @@ public class MemberServiceTest {
 
 
         //then
-        Assert.fail("예외가 발생해야 한다.");
+        fail("예외가 발생해야 한다.");
     }
 
 }
